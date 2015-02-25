0a32bfccab93d2d4a4437d94f7da36aa8b5df2d8
JoaoMarcos <jmra10@gmail.com>
2015-02-25 14:44:43 -0300

    Initial Commit

---
+++ Table/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Table
+//
+//  Created by João Marcos on 25/02/15.
+//  Copyright (c) 2015 João Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table
+//
+//  Created by João Marcos on 25/02/15.
+//  Copyright (c) 2015 João Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
